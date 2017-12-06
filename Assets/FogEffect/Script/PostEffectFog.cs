6b5855065131a0ddfa3d4105900f0db5a4205a3f
lsx6244413 <lsx_xy@126.com>
2017-12-06 21:37:19 +0800

    Add FogEffect

---
+++ Assets/FogEffect/Script/PostEffectFog.cs
@@ -0,0 +1,70 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PostEffectFog : MonoBehaviour {
+
+    public Material material;
+    private Transform m_camTrans;
+    private float camFov;
+    private float camNear;
+    private float camFar;
+    private float camAspect;
+    // Use this for initialization
+    void Awake () {
+        m_camTrans = gameObject.transform;
+        var camCO = gameObject.GetComponent<Camera>();
+        camCO.depthTextureMode |= DepthTextureMode.DepthNormals;
+        camFov = camCO.fieldOfView;
+        camNear = camCO.nearClipPlane;
+        camFar = camCO.farClipPlane;
+        camAspect = camCO.aspect;
+       
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    void OnRenderImage(RenderTexture src, RenderTexture dst)
+    {
+        if (material == null)
+        {
+            Graphics.Blit(src, dst);
+            return;
+        }
+        Matrix4x4 frustumCorners = Matrix4x4.identity;
+
+        float fovWHalf = camFov * 0.5f;
+
+        Vector3 toRight = m_camTrans.right * camNear * Mathf.Tan(fovWHalf * Mathf.Deg2Rad) * camAspect;
+        Vector3 toTop = m_camTrans.up * camNear * Mathf.Tan(fovWHalf * Mathf.Deg2Rad);
+
+        Vector3 topLeft = (m_camTrans.forward * camNear - toRight + toTop);
+        float camScale = topLeft.magnitude * camFar / camNear;
+
+        topLeft.Normalize();
+        topLeft *= camScale;
+
+        Vector3 topRight = (m_camTrans.forward * camNear + toRight + toTop);
+        topRight.Normalize();
+        topRight *= camScale;
+
+        Vector3 bottomRight = (m_camTrans.forward * camNear + toRight - toTop);
+        bottomRight.Normalize();
+        bottomRight *= camScale;
+
+        Vector3 bottomLeft = (m_camTrans.forward * camNear - toRight - toTop);
+        bottomLeft.Normalize();
+        bottomLeft *= camScale;
+
+        frustumCorners.SetRow(0, topLeft);
+        frustumCorners.SetRow(1, topRight);
+        frustumCorners.SetRow(2, bottomRight);
+        frustumCorners.SetRow(3, bottomLeft);
+
+        material.SetMatrix("_FrustumCornersWS", frustumCorners);
+        Graphics.Blit(src, dst, material);
+    }
+}
---
+++ Assets/FogEffect/Shader/PostEffectFog.shader
@@ -0,0 +1,88 @@
+﻿Shader "Unlit/PostEffectFog"
+{
+	Properties
+	{
+		_MainTex ("Texture", 2D) = "white" {}
+		_FogColor("Fog Color", Color) = (0.3, 0.4, 0.7, 1.0)
+		_FogStart("Fog Start", float) = 0
+		_Density("Density", float) = 0
+
+	}
+	SubShader
+	{
+		Tags { "RenderType"="Opaque" }
+		LOD 100
+
+		Pass
+		{
+			CGPROGRAM
+			#pragma vertex vert
+			#pragma fragment frag
+			// make fog work
+			#pragma multi_compile_fog
+			
+			#include "UnityCG.cginc"
+
+			struct appdata
+			{
+				float4 vertex : POSITION;
+				float2 uv : TEXCOORD0;
+			};
+
+			struct v2f
+			{
+				float2 uv : TEXCOORD0;
+				//UNITY_FOG_COORDS(1)
+				float4 vertex : SV_POSITION;
+				float4 interpolatedRay : TEXCOORD1;
+			};
+
+			float _FogStart;
+			float _Density;
+			fixed4 _FogColor;
+			float4 _MainTex_ST;
+			float4 _CameraDepthTexture_ST;
+			float4x4 _FrustumCornersWS;
+			sampler2D _MainTex;
+			sampler2D _CameraDepthTexture;
+			
+			/*v2f vert (appdata v)
+			{
+				v2f o;
+				o.vertex = UnityObjectToClipPos(v.vertex);
+				o.uv = TRANSFORM_TEX(v.uv, _MainTex);
+				UNITY_TRANSFER_FOG(o,o.vertex);
+				return o;
+			}*/
+
+			v2f vert (appdata_img v)
+			{
+				v2f o;
+				o.vertex = UnityObjectToClipPos(v.vertex);
+				o.uv = v.texcoord.xy;
+
+				int xx = (int)v.vertex.x;
+				int yy = (int)v.vertex.y;
+				int z = abs (3 - xx - 3 * yy);
+				o.interpolatedRay = _FrustumCornersWS[z];
+
+				o.interpolatedRay.w = v.vertex.z ;
+
+				return o;
+			}
+			
+			fixed4 frag (v2f i) : SV_Target
+			{
+				// sample the texture
+				//fixed4 col = tex2D(_MainTex, i.uv);
+				// apply fog
+				//UNITY_APPLY_FOG(i.fogCoord, col);
+				float depth = Linear01Depth ( SAMPLE_DEPTH_TEXTURE ( _CameraDepthTexture, UnityStereoScreenSpaceUVAdjust(i.uv, _CameraDepthTexture_ST)));
+				float3 worldPos = ( depth * i.interpolatedRay ).xyz + _WorldSpaceCameraPos;
+
+				return lerp (tex2D (_MainTex, i.uv), _FogColor, saturate(exp(worldPos.y - _FogStart) * _Density));
+			}
+			ENDCG
+		}
+	}
+}
