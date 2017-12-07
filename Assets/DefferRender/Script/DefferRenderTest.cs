d457ccd7fd1346d6834dbe0d1149f75ce1c01d12
lsx6244413 <lsx_xy@126.com>
2017-12-07 11:13:37 +0800

    Add DefferRender

---
+++ Assets/DefferRender/Script/DefferRenderTest.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[ExecuteInEditMode]
+public class DefferRenderTest : MonoBehaviour {
+
+	// Use this for initialization
+    [SerializeField]
+    private Shader shader;
+
+    private Material mat = null;
+
+    private void OnEnable()
+    {
+        
+    }
+
+    void OnRenderImage(RenderTexture source, RenderTexture destination)
+    {
+        if (mat == null)
+        {
+            Graphics.Blit(source, destination);
+            return;
+        }
+
+        Graphics.Blit(source, destination, mat);
+    }
+
+	void Awake () {
+        if (mat == null)
+        {
+            mat = new Material(shader);
+        }
+        var camCO = GetComponent<Camera>();
+        //camCO.depthTextureMode |= DepthTextureMode.DepthNormals;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
