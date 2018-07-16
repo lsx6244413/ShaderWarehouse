218c42b1d192984751666df900878b13f71b4f2f
lsx6244413 <lsx_xy@126.com>
2018-07-16 14:56:31 +0800

    Add HeatEffect Demo

---
+++ Assets/HeatMap/Script/HeatMap.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HeatMap : MonoBehaviour {
+
+    [SerializeField] private int countPoint;
+   [SerializeField] private Material heatMat;
+
+    [SerializeField]
+    private Vector4[] pointPoses;
+    [SerializeField]
+    private Vector4[] pointProper;
+
+    
+    private void Awake()
+    {
+        heatMat.SetFloatArray("_Points", new float[countPoint]);
+    }
+    // Use this for initialization
+    void Start () {
+
+        pointPoses = new Vector4[countPoint];
+        pointProper = new Vector4[countPoint];
+
+        for (int i = 0; i < pointPoses.Length; i++)
+        {
+            pointPoses[i] = new Vector2(Random.Range(-0.9f, 0.9f), Random.Range(-0.5f, 0.5f));
+            pointProper[i] = new Vector2(Random.Range(0f, 0.25f), Random.Range(-0.25f, 1f)); // (Radius, Intensities)
+        }
+
+        heatMat.SetVectorArray("_Properties", pointProper);
+        heatMat.SetInt("_Points_Length", countPoint);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        for (int i = 0; i < countPoint; i++)
+        {
+            pointPoses[i] += new Vector4(Random.Range(-0.1f, +0.1f), Random.Range(-0.1f, +0.1f), 0f, 0f) * Time.deltaTime;
+        }
+        heatMat.SetVectorArray("_Points", pointPoses);
+       // heatMat.SetVectorArray("_Properties", pointProper);
+    }
+}
---
+++ Assets/HeatMap/Shader/Heatmap.shader
@@ -0,0 +1,70 @@
+﻿// Upgrade NOTE: replaced 'mul(UNITY_MATRIX_MVP,*)' with 'UnityObjectToClipPos(*)'
+
+// Credits: Alan Zucconi www.alanzucconi.com
+Shader "Ellioman/Heatmap"
+{
+	Properties
+	{
+		_HeatTex("Texture", 2D) = "white" {}
+	}
+
+	SubShader
+	{
+		Tags{ "Queue" = "Transparent" }
+		Blend SrcAlpha OneMinusSrcAlpha 
+
+		Pass
+		{
+			CGPROGRAM
+			#pragma vertex vert
+			#pragma fragment frag
+
+			struct vertInput
+			{
+				float4 pos : POSITION;
+			};
+
+			struct vertOutput
+			{
+				float4 pos : POSITION;
+				fixed3 worldPos : TEXCOORD1;
+			};
+
+			//自定义变量
+			uniform int _Points_Length = 0;
+			uniform float3 _Points[100];		// (x, y, z) = 位置
+			uniform float2 _Properties[100];	// x = 半径, y = 透明度
+			sampler2D _HeatTex;
+
+			vertOutput vert(vertInput input)
+			{
+				vertOutput o;
+				o.pos = UnityObjectToClipPos(input.pos);
+				o.worldPos = mul(unity_ObjectToWorld, input.pos).xyz;
+				return o;
+			}
+
+			half4 frag(vertOutput output) : COLOR
+			{
+				half h = 0;
+				for (int i = 0; i < _Points_Length; i++)
+				{
+					// 计算每一个分布点位置的距离
+					half di = distance(output.worldPos, _Points[i].xyz);
+
+					half ri = _Properties[i].x;
+					half hi = 1 - saturate(di / ri);
+
+					h += hi * _Properties[i].y;
+				}
+
+				
+				h = saturate(h);
+				half4 col = tex2D(_HeatTex, fixed2(h, 0.5));
+				return col;
+			}
+			ENDCG
+		}
+	}
+	Fallback "Diffuse"
+}
